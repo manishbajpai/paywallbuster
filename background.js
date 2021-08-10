d7e5e62eff9d300eaef796f63b59686fec008093
Manish <manish.baj@gmail.com>
2021-08-09 19:42:48 -0700

    Non working changes to enable parsing a google page
    Further debugging needed to make the changes work.

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     //code in here will run every time a user goes onto a new tab
+
 		if (changeInfo.status === "complete" )     {
 			console.log("Tab loading: " + changeInfo.status)
+
 			chrome.tabs.executeScript(null, {
 				file: 'execute.js'
 			}, _=>{
@@ -12,7 +14,46 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 			});
 		}
 });
-// Listen to messages from the payload.js script and write to popout.html
+console.log("Injected...");
+
+function addwebsite() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        url = new URL(tabs[0].url);
+        hostname = url.hostname
+        console.log(hostname)
+        chrome.storage.sync.set({hostname: "1"}, function() {
+            console.log("Added website");
+        });
+    });
+
+}
+
+function removewebsite() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        url = new URL(tabs[0].url);
+        hostname = url.hostname
+        console.log(hostname)
+        chrome.storage.sync.remove(hostname, function() {
+            console.log("Removed website");
+        });
+    });
+
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        title: "Add website to " + name,
+        contexts: ['all'],
+        onclick: addwebsite
+    });
+    chrome.contextMenus.create({
+        title: "Remove website from " + name,
+        contexts: ['all'],
+        onclick: removewebsite
+    });
+});
+
+// TBD: Listen to messages from the payload.js script and write to popout.html
 chrome.runtime.onMessage.addListener(function (message) {
 	console.log("A message was received from a tab");
 });
--- execute.js
+++ execute.js
@@ -1,5 +1,5 @@
 // send the page title as a chrome message
-
+name = "Ad Aware"
 var searchtext = ["The Mercury News",
                   "The New York Times"]
 
@@ -31,25 +31,118 @@ function replace(a,b)
     }
   }
 }
-if (document.addEventListener) {
-  //DOMContentLoaded doesn't wait for styles, subframes etc.
-  //document.addEventListener("DOMContentLoaded", initializeExecutor(), false);
 
-  document.addEventListener("load", initializeExecutor());
 
+
+/* MutationObserver configuration data: Listen for "childList"
+ * mutations in the specified element and its descendants */
+var config = {
+    childList: true,
+    subtree: true
+};
+var regex = /<a.*?>[^<]*<\/a>/;
+
+/* Traverse 'rootNode' and its descendants and modify '<a>' tags */
+function modifyLinks(rootNode) {
+    var nodes = [rootNode];
+    while (nodes.length > 0) {
+        var node = nodes.shift();
+        console.log("details are: " + node.innerHTML)
+
+        if (node.tagName == "A") {
+            /* Modify the '<a>' element */
+            node.innerHTML = "~~" + node.innerHTML + "~~";
+        } else {
+            /* If the current node has children, queue them for further
+             * processing, ignoring any '<script>' tags. */
+            [].slice.call(node.children).forEach(function(childNode) {
+                if (childNode.tagName != "SCRIPT") {
+                    nodes.push(childNode);
+                }
+            });
+        }
+    }
 }
 
-function initializeExecutor() {
-  console.log("Annoting all the known websites");
+/* Observer1: Looks for 'div.search' */
+var observer1 = new MutationObserver(function(mutations) {
+    /* For each MutationRecord in 'mutations'... */
+    mutations.some(function(mutation) {
+        /* ...if nodes have beed added... */
+        if (mutation.addedNodes && (mutation.addedNodes.length > 0)) {
+            /* ...look for 'div#search' */
+            n = mutation.target
+            console.log("mutation added " + " " + n.tagName)
+            modifyLinks(n)
+            var node = mutation.target.querySelector("div#main");
+            if (node) {
+                console.log("mutation search found")
+
+                /* 'div#search' found; stop observer 1 and start observer 2 */
+                observer1.disconnect();
+                observer2.observe(node, config);
+
+                if (regex.test(node.innerHTML)) {
+                    /* Modify any '<a>' elements already in the current node */
+                    console.log("modifying links")
+                    modifyLinks(node);
+                }
+                return true;
+            }
+        }
+    });
+});
+
+/* Observer2: Listens for '<a>' elements insertion */
+var observer2 = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.addedNodes) {
+            [].slice.call(mutation.addedNodes).forEach(function(node) {
+                /* If 'node' or any of its desctants are '<a>'... */
+                if (regex.test(node.outerHTML)) {
+                    /* ...do something with them */
+                    console.log("modifying links -2 ")
+                    modifyLinks(node);
+                }
+            });
+        }
+    });
+});
+
+
+function filterResultInserts(event) {
+    var str = event.relatedNode;
+    //if (str.includes("search")) {
+        //console.log(event.target.nodeName);
+        //modifyLinks(event.target);
+    //}
+}
+function run() {
+  //console.log("Annoting all the known websites");
+
   setTimeout(function()
     {
       var x;
       for (x in searchtext)
       {
-        replace(searchtext[x], " [$$$] " + searchtext[x]);
+        //replace(searchtext[x], " [$$$] " + searchtext[x]);
       }
     }
     , 3000);
-  console.log("Done Annoting all the known websites");
+  //console.log("Done Annoting all the known websites");
+  /* Start observing 'body' for 'div#search' */
+  //observer1.observe(document.body, config);
+
+
+document.addEventListener('DOMNodeInserted', filterResultInserts);
+
+}
+
+
+if (document.addEventListener) {
+  //DOMContentLoaded doesn't wait for styles, subframes etc.
+  //document.addEventListener("DOMContentLoaded", run(), false);
+
+  document.addEventListener("load", run());
 
 }
